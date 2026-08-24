eeb7bf2cfea4474e7f0bbf7e6f444ef00c622088
ullas-vcpl <ullasraj2@gmail.com>
2026-08-24 11:13:53 +0530

    added updateProduct

--- src/controllers/updateProductController.js
+++ src/controllers/updateProductController.js
@@ -4,8 +4,17 @@ const products = require('../models/products.js');
 const updateProductController = async (req, res) => {
     const { id } = req.params;
     const { name, price, description, productcode, discount } = req.body;
+    updates = {};
+    if(name!==undefined) updates.name = name;
+    if(price!==undefined) updates.price = price;
+    if(description!==undefined) updates.description = description;
+    if(productcode!==undefined) updates.productcode = productcode;
+    if(discount!==undefined) updates.discount = discount;
+    console.log(updates);
+    console.log(req.body);
+    console.log(id);
     try {
-        const updatedProduct = await products.findByIdAndUpdate(id, { name, price, description, productcode, discount }, { new: true });
+        const updatedProduct = await products.findByIdAndUpdate(id, { $set: updates }, { new: true, runValidators: false });
         res.json(updatedProduct);
     } catch (error) {
         res.status(500).json({ message: error.message });
